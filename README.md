a8c385db5d1b2ca8e1f7b4991937c664ecc95bf8
pc156 <pc156742@gmail.com>
2024-05-16 21:45:13 +0530

    //

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # First_repo
+Hello World 
+
+getting started with Linux and Git.
---
+++ hello.java
@@ -0,0 +1,15 @@
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        System.out.println("no to say");
+
+        System.out.println("this is my first repo on github,trying to get better at linux");
+
+
+    }
+}
